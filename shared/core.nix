a0cb0d33ca74e40d70143ae059884783794c6042
Jørgen Gundersen <jg@gundersenj.com>
2026-05-08 08:28:05 +0000

    feat(core): add iputils ping

--- shared/core.nix
+++ shared/core.nix
@@ -1,20 +1,26 @@
-_: {
+{ lib, ... }:
+{
   perSystem =
     { pkgs, ... }:
     {
       devShells.core = pkgs.mkShell {
-        packages = with pkgs; [
-          bubblewrap
-          less
-          jq
-          yq
-          ripgrep
-          gnumake
-          curl
-          wget
-          tree
-          unzip
-        ];
+        packages =
+          with pkgs;
+          [
+            bubblewrap
+            less
+            jq
+            yq
+            ripgrep
+            gnumake
+            curl
+            wget
+            tree
+            unzip
+          ]
+          ++ lib.optionals pkgs.stdenv.isLinux [
+            iputils
+          ];
       };
     };
 }
